b57d5abc9340ac0a0bb13ec7e519bf391a343325
Bechirayari <124601456+Bechirayari@users.noreply.github.com>
2023-02-26 18:51:02 +0100

    Add files via upload

---
+++ checkpoint.js
@@ -0,0 +1,12 @@
+Input: n pairs of vectors v1 and v2, each of length m
+Output: Whether or not each pair of vectors is orthogonal
+1.     For each pair of vectors do steps 2-3
+2.     Call the dot_product procedure with v1 and v2 as arguments, store the result in variable ps
+3.     If ps is zero, print "v1 and v2 are orthogonal", else print "v1 and v2 are not orthogonal"
+
+Input: n pairs of vectors v1 and v2, each of length m
+Output: Whether or not each pair of vectors is orthogonal
+
+1. For each pair of vectors do steps 2-3
+2.     Call the dot_product procedure with v1 and v2 as arguments, store the result in variable ps
+3.     If ps is zero, print "v1 and v2 are orthogonal", else print "v1 and v2 are not orthogonal"
